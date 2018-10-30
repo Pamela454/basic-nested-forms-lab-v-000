1cb20e6594189a6e8a911ac5bbf1a1035f9145d6
Pamela Frieze <pfrieze@gmail.com>
2018-10-30 19:09:42 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,12 @@ class RecipesController < ApplicationController
     redirect_to recipe
   end
 
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.update(recipe_params)
+    redirect_to recipe
+  end
+
   def recipe_params
     params.require(:recipe).permit(:title, ingredients: [:id, :name, :quantity])
     end
